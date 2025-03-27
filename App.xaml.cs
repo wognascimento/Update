eaf5091d82aa950898632b72ac23b7ac6d8013cc
wesley <wog.nascimento@gmail.com>
2025-03-27 14:31:18 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Text.Json;
+using System.Windows;
+
+namespace Update
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Em ambiente de desenvolvimento, simule os parâmetros caso não haja
+            string[] args = e.Args;
+
+            //{"currentVersion":"1.1.0.0","updateVersion":"1.1.0.0","updateUrl":"http://192.168.0.49/downloads/central-sig/application-1.1.0.0.zip","changelog":["Corre\u00C3\u00A7\u00C3\u00A3o de bugs","Melhorias de desempenho"],"releaseDate":"2025-03-26","minimumCompatibleVersion":"1.0.0"}
+#if DEBUG
+            if (args == null || args.Length == 0)
+            {
+                //args = ["{\"currentVersion\":\"1.1.0.0\",\"updateVersion\":\"1.1.0.0\",\"updateUrl\":\"http://192.168.0.49/downloads/central-sig/application-1.1.0.0.zip\",\"changelog\":[\"Corre\\u00C3\\u00A7\\u00C3\\u00A3o de bugs\",\"Melhorias de desempenho\"],\"releaseDate\":\"2025-03-26\",\"minimumCompatibleVersion\":\"1.0.0\"}"];
+
+                args = new string[] { "{\"currentVersion\":\"1.1.0.0\",\"updateVersion\":\"1.1.0.0\",\"updateUrl\":\"http://192.168.0.49/downloads/central-sig/application-1.1.0.0.zip\",\"changelog\":[\"Corre\\u00C3\\u00A7\\u00C3\\u00A3o de bugs\",\"Melhorias de desempenho\"],\"releaseDate\":\"2025-03-26\",\"minimumCompatibleVersion\":\"1.0.0\"}", "CentralSIG.exe" };
+            }
+#endif
+            
+
+            if (args.Length > 0)
+            {
+                string jsonString = args[0];
+                try
+                {
+
+                    //JObject jsonObj = JsonConvert.DeserializeObject<JObject>(jsonString);
+                    // Acesse os valores do JSON, por exemplo:
+                    //string nome = jsonObj["nome"]?.ToString();
+                    //string modo = jsonObj["modo"]?.ToString();
+                    //Console.WriteLine($"Nome: {nome}, Modo: {modo}");
+
+                    //MessageBox.Show(@$"ARGUMENTO: {jsonString}");
+
+                    // Cria a instância da MainWindow
+                    MainWindow mainWindow = new()
+                    {
+                        // Define o valor que será passado
+                        UpdateInfoJson = jsonString,
+                        AplicacaoPrincipal = args[1]
+                    };
+
+                    // Mostra a janela
+                    mainWindow.Show();
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro ao processar o JSON: " + ex.Message);
+                }
+            }
+            
+
+            // Prossegue com a inicialização da aplicação
+            base.OnStartup(e);
+        }
+    }
+
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using BibliotecasSIG;
+using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
+using System.Windows;
+
+namespace Update
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string UpdateInfoJson { get; set; }
+        public string AplicacaoPrincipal { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var updateInfo = JsonSerializer.Deserialize<UpdateInfo>(UpdateInfoJson);
+            var updateChecker = new UpdateChecker();
+
+            try
+            {
+                // Caminho para salvar o download (pode ser personalizado)
+                string downloadPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Update", $"application-{updateInfo.updateVersion}.zip");
+                // Cria diretório se não existir
+                Directory.CreateDirectory(Path.GetDirectoryName(downloadPath));
+                // Cria um progress handler que atualiza o ProgressBar
+                var progressHandler = new Progress<double>(value =>
+                {
+                    progressDownload.Value = value;
+                });
+
+                // Chama o método de download passando o progressHandler
+                bool sucesso = await updateChecker.DownloadUpdateAsync(updateInfo.updateUrl, downloadPath, progressHandler);
+
+                if (sucesso)
+                {
+                    var progressHandlerExtraction = new Progress<double>(valor =>
+                    {
+                        progressExtraction.Value = valor;
+                    });
+                    
+                    bool sucessoExtraction = await updateChecker.ExtrairZipComProgressoAsync(downloadPath, progressHandlerExtraction);
+
+                    if (sucessoExtraction)
+                    {
+                        Process.Start(AplicacaoPrincipal);
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Falha ao extrair o arquivo.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Falha ao realizar o download.");
+                }
+
+            }
+            catch(JsonException ex)
+            {
+                // Log do erro ou tratamento de exceção
+                MessageBox.Show(
+                    $"Erro ao converter JSON para MODEL: {ex.Message}",
+                    "Erro",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error
+                );
+            }
+            catch (Exception ex)
+            {
+                // Log do erro ou tratamento de exceção
+                MessageBox.Show(
+                    $"Erro ao verificar atualizações: {ex.Message}",
+                    "Erro",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error
+                );
+            }
+
+        }
+    }
+}
